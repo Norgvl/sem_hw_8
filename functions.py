394a776063d9d6f825d43641cda8361187f99a22
vlad <norg.vl@mail.ru>
2023-05-29 14:44:26 +0300

    up

--- functions.py
+++ functions.py
@@ -15,16 +15,21 @@ def add_data() -> None:
 def find_data() -> None:
     """Печатает результат поиска по справочнику."""
     with open('book.txt', 'r', encoding='utf-8') as file:
-        data = file.read()
-    print(data)
+        data = file.read().split('\n')
+    print('\n'.join(data))
     data_to_find = input('Введите данные для поиска: ')
-    print(search(data, data_to_find))
+    print(f'\n {search(data, data_to_find)} \n')
 
 
-def search(book: str, info: str) -> str:
+def search(book: list[str], info: str) -> str:
     """Находит в списке записи по определенному критерию поиска"""
-    book = book.split('\n')
-    for contact in book:
-        if info in contact:
-            return contact
-    return 'Совпадений не найдено'
+    find = [i for i in book if info in i]
+    if len(find) == 0:
+        return 'Ничего не найдено'
+    elif len(find) == 1:
+        return find[0]
+    elif len(find) > 1:
+        print()
+        print('Нашлось несколько результатов')
+        info_1 = input('Уточните поиск: ')
+        return search(book, info_1)
